482a5ebf9f0ae95d53e60bd257fbf40d54d5ca0b
Keah Logistics <keahlogisticsq@gmail.com>
2026-04-21 21:32:18 +0100

    Fxier

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path' // You might need to install @types/node
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  // Add this block to fix the "process is not defined" error
+  resolve: {
+    alias: {
+      // This redirects 'crypto' imports to the browser-safe version
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+    },
+  },
   define: {
     global: 'window',
     'process.env': {},
