b108f368722acbc9dd8eb6b0bf572871047c8636
Keah Logistics <keahlogisticsq@gmail.com>
2026-06-06 15:58:49 +0100

    Fxi

--- api/index.js
+++ api/index.js
@@ -86,19 +86,10 @@ app.use(cors(corsOptions));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.disable('x-powered-by');
 
-const jsonParser = express.json({ limit: '5mb' });
-const urlEncodedParser = express.urlencoded({ limit: '5mb', extended: true });
+// ADD THESE INSTEAD
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
-app.use((req, res, next) => {
-  const contentType = req.headers['content-type'] || '';
-  if (contentType.includes('application/json')) {
-    return jsonParser(req, res, next);
-  }
-  if (contentType.includes('application/x-www-form-urlencoded')) {
-    return urlEncodedParser(req, res, next);
-  }
-  next();
-});
 // --- 3. PARSING ERROR HANDLER ---
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
